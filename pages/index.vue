be66b4baa316cd3c1b6e2fa1ab13439fa785965c
Natalia Makarchuk <natalia.makarchuk@sombrainc.com>
2019-10-30 13:13:32 +0200

    init app

---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <h1>WI-FI Accesses</h1>
+    <div>If you want to connect to WI-FI please click "Login" button</div>
+    <button class="login-button">Login</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style>
+  .container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+  }
+
+  .login-button {
+    width: 200px;
+  }
+</style>
+
+
+
